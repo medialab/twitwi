7944d42dc1c40f05df1caabf5b887268e960953e
Yomguithereal <guillaumeplique@gmail.com>
2021-01-30 10:40:57 +0100

    Scaffolding formatters file

---
+++ twitwi/formatters.py
@@ -0,0 +1,7 @@
+# =============================================================================
+# Twitwi Formatters
+# =============================================================================
+#
+# Miscellaneous formatter functions transforming tweets from one format
+# to the other.
+#
