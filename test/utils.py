6c7a5e3f696b629225cb8bd7d70e0c206590774e
Yomguithereal <guillaumeplique@gmail.com>
2021-02-10 13:31:34 +0100

    Moving tests to resources

--- test/utils.py
+++ test/utils.py
@@ -1,6 +1,7 @@
 # =============================================================================
 # Twitwi Unit Test Utilities
 # =============================================================================
+import json
 from os.path import join, dirname
 
 RESOURCES_DIR = join(dirname(__file__), 'resources')
@@ -8,3 +9,18 @@ RESOURCES_DIR = join(dirname(__file__), 'resources')
 
 def open_resource(name):
     return open(join(RESOURCES_DIR, name))
+
+
+def open_json_resource(name):
+    with open(join(RESOURCES_DIR, name)) as f:
+        return json.load(f)
+
+
+def omit(o, keys):
+    c = {}
+
+    for k, v in o.items():
+        if k not in keys:
+            c[k] = v
+
+    return c
