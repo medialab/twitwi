de522dae1f9fc7127a903019fbfe0de6ce4d5407
bmaz <beatrice.mazoyer@gmail.com>
2021-02-11 11:05:37 +0100

    add custom_normalize_url to __init__

--- twitwi/__init__.py
+++ twitwi/__init__.py
@@ -4,4 +4,4 @@
 #
 from twitwi.client_wrapper import TwitterWrapper
 from twitwi.formatters import transform_tweet_into_csv_dict
-from twitwi.utils import get_dates, normalize_tweet
+from twitwi.utils import get_dates, normalize_tweet, custom_normalize_url
