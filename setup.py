56ea8d0f69efdcf45c87b475a3d006faaf96bbfb
Yomguithereal <guillaumeplique@gmail.com>
2025-03-26 11:56:06 +0100

    python >=3.8

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup, find_packages
 
-with open('./README.md', 'r') as f:
+with open("./README.md", "r") as f:
     long_description = f.read()
 
-setup(name='twitwi',
-      version='0.20.0',
-      description='A collection of Twitter-related helper functions for python.',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='http://github.com/medialab/twitwi',
-      license='MIT',
-      author='Béatrice Mazoyer, Guillaume Plique, Benjamin Ooghe-Tabanou',
-      author_email='guillaume.plique@sciencespo.fr',
-      keywords='twitter',
-      python_requires='>=3.4',
-      packages=find_packages(exclude=['scripts', 'test']),
-      package_data={'docs': ['README.md']},
-      install_requires=[
-        'pytz>=2019.3',
-        'ural>=0.31.1'
-      ],
-      zip_safe=True)
+setup(
+    name="twitwi",
+    version="0.20.0",
+    description="A collection of Twitter-related helper functions for python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/medialab/twitwi",
+    license="MIT",
+    author="Béatrice Mazoyer, Guillaume Plique, Benjamin Ooghe-Tabanou",
+    author_email="guillaume.plique@sciencespo.fr",
+    keywords="twitter",
+    python_requires=">=3.8",
+    packages=find_packages(exclude=["scripts", "test"]),
+    package_data={"docs": ["README.md"]},
+    install_requires=["pytz>=2019.3", "ural>=0.31.1"],
+    zip_safe=True,
+)
