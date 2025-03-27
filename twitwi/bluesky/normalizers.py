80acd6f97976fdd191ec064b781eefc630037ff9
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2025-03-27 16:18:47 +0100

    [WIP] normalize bluesky posts

--- twitwi/bluesky/normalizers.py
+++ twitwi/bluesky/normalizers.py
@@ -1,7 +1,7 @@
 from typing import Dict, Union
 
-from twitwi.utils import get_dates
-from twitwi.bluesky.types import BlueskyProfile
+from twitwi.utils import get_collection_time, get_dates
+from twitwi.bluesky.types import BlueskyProfile, BlueskyPost
 
 
 def normalize_profile(data: Dict, locale: Union[str, None] = None) -> BlueskyProfile:
@@ -32,3 +32,47 @@ def normalize_profile(data: Dict, locale: Union[str, None] = None) -> BlueskyPro
         "banner": data["banner"],
         "pinned_post_uri": pinned_post_uri,
     }
+
+
+def parse_post_uri(uri):
+    if not uri.startswith("at://") and "/app.bsky.feed.post/" not in uri:
+        raise(Exception(f"Not a BlueSky post uri: {uri}"))
+    pieces = uri[5:].split("/app.bsky.feed.post/")
+    return {
+        "user_did": pieces[0],
+        "post_did": pieces[1]
+    }
+
+
+def format_post_url(user_handle, post_did):
+    return f"https://bsky.app/profile/{user_handle}/post/{post_did}"
+
+
+def normalize_post(data: Dict, locale: Union[str, None] = None) -> BlueskyPost:
+
+    post = {}
+
+    post["collection_time"] = get_collection_time()
+
+    post["cid"] = data["cid"]
+    post["uri"] = data["uri"]
+
+    post_dids = parse_post_uri(data["uri"])
+    post["did"] = post_dids["post_did"]
+
+    post["timestamp_utc"], post["local_time"] = get_dates(data["record"]["createdAt"], locale=locale, source="bluesky")
+
+    post["user_did"] = post_dids["user_did"]
+    if post["user_did"] != data["author"]["did"]:
+        raise(Exception(f"Inconsistent user did between BlueSky post uri and post's author metadata: {data['uri']}"))
+
+    post["user_handle"] = data["author"]["handle"]
+    post["user_name"] = data["author"]["displayName"]
+    post["user_image"] = data["author"]["avatar"]
+    post["user_timestamp_utc"], post["user_created_at"] = get_dates(data["author"]["createdAt"], locale=locale, source="bluesky")
+    post["user_langs"] = data["record"]["langs"]
+
+    post["url"] = format_post_url(post["user_handle"], post["did"])
+
+    return post
+
--- twitwi/bluesky/types.py
+++ twitwi/bluesky/types.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, Optional
+from typing import TypedDict, List, Optional
 
 
 class BlueskyProfile(TypedDict):
@@ -17,3 +17,55 @@ class BlueskyProfile(TypedDict):
     avatar: str
     banner: str
     pinned_post_uri: Optional[str]
+
+
+class BlueskyPost(TypedDict):
+    cid: str
+    did: str
+    url: str
+    uri: str
+    timestamp_utc: int
+    local_time: str
+    user_did: str
+    user_handle: str
+    text: str
+    repost_count: int
+    like_count: int
+    reply_count: int
+    quote_count: int
+    lang: str
+    user_name: str
+    # user_description: str
+    # user_url: str
+    user_image: str
+    # user_posts: int
+    # user_followers: int
+    # user_friends: int
+    # user_likes: int
+    user_created_at: str
+    user_timestamp_utc: int
+    user_langs: List[str]
+    to_user_handle: str
+    to_user_did: str
+    to_post_did: str
+    to_root_post_did: str
+    reposted_did: str
+    reposted_user_handle: str
+    reposted_user_did: str
+    reposted_timestamp_utc: int
+    quoted_did: str
+    quoted_user_handle: str
+    quoted_user_did: str
+    quoted_timestamp_utc: int
+    links: List[str]
+    domains: List[str]
+    media_urls: List[str]
+    media_files: List[str]
+    media_types: List[str]
+    media_alt_texts: List[str]
+    mentioned_user_handles: List[str]
+    mentioned_user_dids: List[str]
+    hashtags: List[str]
+    collection_time: Optional[str]
+    collected_via: Optional[List[str]]
+    match_query: Optional[str]
