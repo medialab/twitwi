af53739a21ac83041d6abd803674ba90be8e6cd8
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2025-03-28 15:44:09 +0100

    minimalist formatter for posts

--- twitwi/bluesky/constants.py
+++ twitwi/bluesky/constants.py
@@ -1,3 +1,9 @@
-from twitwi.bluesky.types import BlueskyProfile
+from typing import List
+
+from twitwi.bluesky.types import BlueskyProfile, BlueskyPost
 
 PROFILE_FIELDS = list(BlueskyProfile.__annotations__.keys())
+
+POST_FIELDS = list(BlueskyPost.__annotations__.keys())
+
+POST_LIST_FIELDS = [k for k, v in BlueskyPost.__annotations__.items() if v == List[str]]
--- twitwi/bluesky/formatters.py
+++ twitwi/bluesky/formatters.py
@@ -1,8 +1,13 @@
 from typing import List
 
-from twitwi.bluesky.constants import PROFILE_FIELDS
-from twitwi.bluesky.types import BlueskyProfile
+from twitwi.bluesky.constants import PROFILE_FIELDS, POST_FIELDS, POST_LIST_FIELDS
+from twitwi.bluesky.types import BlueskyProfile, BlueskyPost
 
 
 def format_profile_as_csv_row(data: BlueskyProfile) -> List:
     return [data.get(field, "") for field in PROFILE_FIELDS]
+
+
+def format_post_as_csv_row(data: BlueskyPost) -> List:
+    # TODO: handle other field types than str, maybe reuse formater functions from twitwi/formatters
+    return [data.get(field, "") for field in POST_FIELDS if field not in POST_LIST_FIELDS]
