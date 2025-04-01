2759fefb586dd3e71d933268f75a8c1425451266
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2025-04-01 18:57:23 +0200

    fix profiles tests + linter

--- twitwi/bluesky/normalizers.py
+++ twitwi/bluesky/normalizers.py
@@ -173,7 +173,7 @@ def normalize_post(
             hashtags.add(feat["tag"].strip().lower())
 
         elif feat["$type"].endswith("#mention"):
-            if not feat["did"] in post["mentioned_user_dids"]:
+            if feat["did"] not in post["mentioned_user_dids"]:
                 post["mentioned_user_dids"].append(feat["did"])
                 handle = (
                     text[facet["index"]["byteStart"] + 1 : facet["index"]["byteEnd"]]
