953e1b7ed4eaab7b279a6de4a45f5cdb108bae4b
echevale <evan.chevalerias@gmail.com>
2025-10-14 09:35:12 +0200

    Added a new mime type of media within bsky normalize posts

--- twitwi/bluesky/utils.py
+++ twitwi/bluesky/utils.py
@@ -117,7 +117,7 @@ def format_media_url(user_did, media_cid, mime_type, source):
         media_thumb = (
             f"https://video.bsky.app/watch/{user_did}/{media_cid}/thumbnail.jpg"
         )
-    elif mime_type == "application/octet-stream":
+    elif mime_type in ["application/octet-stream", "text/plain"]:
         media_url = (
             f"https://cdn.bsky.app/img/feed_fullsize/plain/{user_did}/{media_cid}@jpeg"
         )
