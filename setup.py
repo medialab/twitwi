3ceb7febfb227f9c7d97919f3e5d98c34bf50501
Yomguithereal <guillaumeplique@gmail.com>
2026-02-11 16:38:23 +0100

    Bump 0.25.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as f:
 
 setup(
     name="twitwi",
-    version="0.24.0",
+    version="0.25.0",
     description="A collection of Twitter-related helper functions for python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
